bfbf7271eb1b956d3c84d7bb3d38011c2f0760fd
kimchulyeon <guinness987@gmail.com>
2023-08-11 17:32:39 +0900

    feat: 카카오맵 설정

---
+++ wtd/KakaoBrideHeader.h
@@ -0,0 +1,13 @@
+//
+//  KakaoBrideHeader.h
+//  wtd
+//
+//  Created by chulyeon kim on 2023/08/11.
+//
+
+#ifndef KakaoBrideHeader_h
+#define KakaoBrideHeader_h
+
+#import <DaumMap/MTMapView.h>
+
+#endif /* KakaoBrideHeader_h */
