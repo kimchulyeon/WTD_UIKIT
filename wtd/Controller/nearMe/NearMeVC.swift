954cbbb0ec53807beaa592c375f8898ada6ca43f
kimchulyeon <guinness987@gmail.com>
2023-08-11 17:54:42 +0900

    feat: 지도탭 세팅 중

--- wtd/Controller/nearMe/NearMeVC.swift
+++ wtd/Controller/nearMe/NearMeVC.swift
@@ -8,17 +8,39 @@
 import UIKit
 
 class NearMeVC: UIViewController {
+    //MARK: - properties ==================
+    var mapView: MTMapView?
     
     //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
      
-        print("NEAR ME VC")
+        CommonUtil.configureBasicView(for: self)
+        configureMapView()
     }
-    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        print("NEARME VC VIEW WILL APPEAR")
+        navigationController?.navigationBar.isHidden = true
     }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        navigationController?.navigationBar.isHidden = false
+    }
+}
+
+extension NearMeVC {
+    private func configureMapView() {
+        mapView = MTMapView(frame: view.bounds)
+        guard let mapView = mapView else { return }
+        mapView.delegate = self
+        mapView.baseMapType = .standard
+        view.addSubview(mapView)
+    }
+}
+
+extension NearMeVC: MTMapViewDelegate {
+    
 }
