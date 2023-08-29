ace78fa3cc518b3ea39e48009ca78d3944922062
kimchulyeon <guinness987@gmail.com>
2023-08-29 15:49:39 +0900

    fix: 파이어베이스 서비스 코드 정리

--- wtd/Service/FirebaseService.swift
+++ wtd/Service/FirebaseService.swift
@@ -12,14 +12,22 @@ import FirebaseAuth
 let DB = Firestore.firestore()
 
 final class FirebaseService {
+    //MARK: - properties ==================
     static let shared = FirebaseService()
     private init() { }
 
     let USER_COL = DB.collection("users")
+}
 
+
+//MARK: - func ==================
+extension FirebaseService {
     /// 파이어베이스 로그인 + 기존/신규 유저 체크
-    func loginFirebase(credential: AuthCredential, completion: @escaping (_ uid: String?, _ isNewUser: Bool, _ docID: String?) -> Void) {
+    func loginFirebase(credential: AuthCredential, completion: @escaping (_ uid: String?,
+                                                                          _ isNewUser: Bool,
+                                                                          _ docID: String?) -> Void) {
         Auth.auth().signIn(with: credential) { [weak self] result, error in
+            guard let weakSelf = self else { return }
             if let error = error {
                 print("Error \(error.localizedDescription) :::::::: ❌")
                 completion(nil, true, nil)
@@ -31,7 +39,7 @@ final class FirebaseService {
                 return
             }
 
-            self?.checkAlreadySignedIn(with: uid) { docID in
+            weakSelf.checkAlreadySignedIn(with: uid) { docID in
                 if let docID = docID {
                     // 기존 유저
                     completion(uid, false, docID)
@@ -63,8 +71,13 @@ final class FirebaseService {
         }
     }
 
-    /// 이름 | 이메일 | uid | createdAt 데이터베이스에 저장
-    func saveUserInDatabase(name: String, email: String, uid: String, provider: String, completion: @escaping (_ docID: String) -> Void) {
+    /// 이름 | 이메일 | uid | provider | createdAt 데이터베이스에 저장
+    func saveUserInDatabase(name: String,
+                            email: String,
+                            uid: String,
+                            provider: String,
+                            completion: @escaping (_ docID: String) -> Void)
+    {
         let DOC = USER_COL.document()
         let DOC_ID = DOC.documentID
 
@@ -91,7 +104,8 @@ final class FirebaseService {
                                                                 _ email: String,
                                                                 _ uid: String,
                                                                 _ docID: String,
-                                                                _ provider: String) -> Void) {
+                                                                _ provider: String) -> Void)
+    {
         USER_COL.document(docID).getDocument { snapshot, error in
             if let error = error {
                 print("Error \(error.localizedDescription) :::::::: ❌")
@@ -103,8 +117,8 @@ final class FirebaseService {
                 let email = snapshot.get(FirestoreFieldConstant.Email.rawValue) as? String,
                 let uid = snapshot.get(FirestoreFieldConstant.Uid.rawValue) as? String,
                 let docID = snapshot.get(FirestoreFieldConstant.DocID.rawValue) as? String,
-                let provider = snapshot.get(FirestoreFieldConstant.DocID.rawValue) as? String {
-
+                let provider = snapshot.get(FirestoreFieldConstant.DocID.rawValue) as? String
+            {
                 completion(name, email, uid, docID, provider)
             }
         }
@@ -112,9 +126,7 @@ final class FirebaseService {
 
     /// 닉네임 변경
     func changeNicknameInDatabase(with docID: String, newValue: String, completion: @escaping (_ success: Bool) -> Void) {
-        USER_COL.document(docID).updateData([
-            FirestoreFieldConstant.Name.rawValue: newValue
-        ]) { error in
+        USER_COL.document(docID).updateData([FirestoreFieldConstant.Name.rawValue: newValue]) { error in
             if let error = error {
                 print("Error while changing nickname with \(error) :::::::❌")
                 completion(false)
@@ -125,7 +137,7 @@ final class FirebaseService {
     }
 
     /// 데이터베이스 삭제
-    func deleteUserDatabaseInfoForLeaving(with docID: String, completion: @escaping (Bool) -> Void) {
+    private func deleteUserDatabaseInfoForLeaving(with docID: String, completion: @escaping (Bool) -> Void) {
         USER_COL.document(docID).delete { error in
             if let error = error {
                 print("Error while delete database for leaving with \(error) :::::::❌")
@@ -150,19 +162,28 @@ final class FirebaseService {
     func leave(completion: @escaping () -> Void) {
         guard let docID = UserDefaultsManager.shared.getUserDefaultData(field: .DocID) else { return }
         deleteUserDatabaseInfoForLeaving(with: docID) { isSucceed in
-            if isSucceed {
-                let currentUser = Auth.auth().currentUser
-                currentUser?.delete(completion: { error in
-                    if let error = error {
-                        print("Error while leaving with \(error) :::::::❌")
-                        CommonUtil.showAlert(title: "재인증 필요", message: "다시 로그인 후 탈퇴를 진행해주세요", actionTitle: "확인", actionStyle: .default) { _ in
-                            CommonUtil.changeRootView(to: LoginVC())
-                        }
-                    } else {
-                        completion()
-                    }
-                })
+            guard isSucceed == true else { return }
+            let currentUser = Auth.auth().currentUser
+            currentUser?.delete { [weak self] error in
+                guard let weakSelf = self else { return }
+                if let error = error {
+                    print("Error while leaving with \(error) :::::::❌")
+                    weakSelf.moveToLoginVCForReAuth()
+                } else {
+                    completion()
+                }
             }
+        }
+    }
+
+    /// 탈퇴하기 위해 재인증 필요 => 알럿 + 로그인 화면으로 이동
+    private func moveToLoginVCForReAuth() {
+        CommonUtil.showAlert(title: "재인증 필요",
+                             message: "다시 로그인 후 탈퇴를 진행해주세요",
+                             actionTitle: "확인",
+                             actionStyle: .default)
+        { _ in
+            CommonUtil.changeRootView(to: LoginVC())
         }
     }
 }
