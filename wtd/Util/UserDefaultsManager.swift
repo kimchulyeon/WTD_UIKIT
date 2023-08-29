637b66bb3be52f527564f34b0047875db77fd143
kimchulyeon <guinness987@gmail.com>
2023-08-29 16:11:20 +0900

    fix: UserDefaultsManager 코드 정리

--- wtd/Util/UserDefaultsManager.swift
+++ wtd/Util/UserDefaultsManager.swift
@@ -9,9 +9,13 @@ import Foundation
 import FirebaseFirestore
 
 final class UserDefaultsManager {
+    //MARK: - properties ==================
     static let shared = UserDefaultsManager()
     private init() { }
+}
 
+//MARK: - func ==================
+extension UserDefaultsManager {
     /// 로그인 유저 정보 저장
     func saveUserInfo(name: String, email: String, docID: String, uid: String, provider: String, completion: @escaping () -> Void) {
         UserDefaults.standard.set(name, forKey: FirestoreFieldConstant.Name.rawValue)
@@ -19,20 +23,19 @@ final class UserDefaultsManager {
         UserDefaults.standard.set(docID, forKey: FirestoreFieldConstant.DocID.rawValue)
         UserDefaults.standard.set(uid, forKey: FirestoreFieldConstant.Uid.rawValue)
         UserDefaults.standard.set(provider, forKey: FirestoreFieldConstant.Provider.rawValue)
-
         completion()
     }
-    
+
     /// UserDefaults에서 데이터 가져오기
     func getUserDefaultData(field: FirestoreFieldConstant) -> String? {
         return UserDefaults.standard.string(forKey: field.rawValue)
     }
-    
+
     /// 게스트인지
     func isGuest() -> Bool {
         return UserDefaultsManager.shared.getUserDefaultData(field: .Name) == nil
     }
-    
+
     /// UserDefaults 비우기 (로그아웃)
     func resetUserDefaults(completion: @escaping () -> Void) {
         if let bundleID = Bundle.main.bundleIdentifier {
